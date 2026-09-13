10c634ab6ee3250fbf361981df362a0974a59e9d
VISAGAN G <visagaan26@gmail.com>
2026-09-13 18:48:55 +0530

    Deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:'/vartaaa/',
+  base: '/varataaa/',
   server: {
     proxy: {
       '/api': {
